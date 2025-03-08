45f2f075e263b50ddc9733fd8fc324e514b9d777
Yashwanth Chiluka <yashwanthchiluka@Yashwanths-MacBook-Pro.local>
2025-03-08 16:15:05 -0600

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+const Contact = () => {
+  return (
+    <section id="contact" className="content-section">
+      <h2>Contact Me</h2>
+      <a href="mailto:your@email.com" className="contact-button">Get in Touch</a>
+    </section>
+  );
+};
+
+export default Contact;
